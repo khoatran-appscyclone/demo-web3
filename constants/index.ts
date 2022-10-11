cf0fa94d9d42ae7bcf4c001031f1531cb4b956be
khoa.tran <khoa.tran@apps-cyclone.com>
2022-10-11 16:27:59 +0700

    auto switch bsc test net chain

---
+++ constants/index.ts
@@ -0,0 +1,18 @@
+export const BSC_CHAIN_ADD_TO_METAMASK = {
+  chainId: 97,
+  chainName: "Binance Smart Chain Testnet",
+  nativeCurrency: {
+    name: "Binance Chain Native Token",
+    symbol: "tBNB",
+    decimals: 18,
+  },
+  rpcUrls: [
+    "https://data-seed-prebsc-1-s1.binance.org:8545",
+    "https://data-seed-prebsc-2-s1.binance.org:8545",
+    "https://data-seed-prebsc-1-s2.binance.org:8545",
+    "https://data-seed-prebsc-2-s2.binance.org:8545",
+    "https://data-seed-prebsc-1-s3.binance.org:8545",
+    "https://data-seed-prebsc-2-s3.binance.org:8545",
+  ],
+  blockExplorerUrls: ["https://testnet.bscscan.com"],
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import { Container, Flex, Button, Box, useToast } from "@chakra-ui/react";
 import type { NextPage } from "next";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Web3 from "web3";
 import CHAINS from "@/constants/chains.json";
 import MetamaskIcon from "@/assets/svg/metamask.svg";
 import DisconnectIcon from "@/assets/svg/disconnect.svg";
 import Image from "next/image";
+import { BSC_CHAIN_ADD_TO_METAMASK } from "@/constants/index";
 
 const Home: NextPage = () => {
   const [connected, setConnected] = useState(false);
@@ -15,19 +16,41 @@ const Home: NextPage = () => {
 
   const toast = useToast();
 
+  const swithChain = async (hexChainId: string) => {
+    await window["ethereum"].request({
+      method: "wallet_switchEthereumChain",
+      params: [{ chainId: hexChainId }],
+    });
+  };
+
+  const addChain = async (chain: Record<string, any>) => {
+    try {
+      await window["ethereum"].request({
+        method: "wallet_addEthereumChain",
+        params: [{ ...chain }],
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const handleConnect = async () => {
     if (window["ethereum"]) {
       const web3 = new Web3(Web3.givenProvider);
       const accounts = await web3.eth.requestAccounts();
       setAccount(accounts[0]);
-      const chainId = await web3.eth.getChainId();
-      const chain = CHAINS.find((x) => x.chainId == chainId);
-      setSymbol(chain?.nativeCurrency.symbol || "");
+      const hexChainId = web3.utils.toHex(BSC_CHAIN_ADD_TO_METAMASK.chainId);
+      try {
+        await swithChain(hexChainId);
+      } catch {
+        await addChain({ ...BSC_CHAIN_ADD_TO_METAMASK, chainId: hexChainId });
+      }
+      setSymbol(BSC_CHAIN_ADD_TO_METAMASK.nativeCurrency.symbol || "");
       const balance = await web3.eth.getBalance(accounts[0]);
       setBalanceOf(web3.utils.fromWei(balance, "ether"));
       toast({
         title: "Connected Wallet",
-        description: `${chain?.name}`,
+        description: `${BSC_CHAIN_ADD_TO_METAMASK.chainName}`,
         status: "success",
         duration: 1000,
         isClosable: true,
@@ -37,7 +60,7 @@ const Home: NextPage = () => {
     } else {
       toast({
         title: "Please install Metamask extension",
-        status: "success",
+        status: "warning",
         duration: 1000,
         isClosable: true,
         position: "top-right",
